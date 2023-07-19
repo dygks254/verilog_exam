b2bbc3ce9058bbb7c49908d75525b5cdbc3e948c
YohabPark <dyqkr@naver.com>
2023-07-20 03:59:22 +0900

    Change makefile and 0-0-exam

--- hdlbits-exam/0.Getting-started/0.Getting-Started/sim_main.cpp
+++ hdlbits-exam/0.Getting-started/0.Getting-Started/sim_main.cpp
@@ -4,4 +4,17 @@
 int main(int argc, char** argv){
 
 
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop* top = new Vtop{contextp};
+
+    while (!contextp->gotFinish()) {
+        top->eval();
+    }
+
+    top->final();
+    
+    delete top;
+    return 0;
+
 }
---
+++ hdlbits-exam/0.Getting-started/0.Getting-Started/top.sv
@@ -0,0 +1,17 @@
+`include "top_module.v"
+`timescale 1ps/1ps
+
+module top;
+
+  wire a;
+
+  top_module src( a );
+
+  initial begin
+    $display("Output value : 0x%0h", a);
+    #10;
+    $display("Output value : 0x%0h", a);
+    $finish;
+  end
+
+endmodule
