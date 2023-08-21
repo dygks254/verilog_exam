62be7885599046db64ab8844b45d006b2e54ee81
YohabPark <dyqkr@naver.com>
2023-08-21 23:56:59 +0900

    Add group 10 verilog 7-13

---
+++ hdlbits-exam/group-10/exam10-13/top_module.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output reg Q); 
+
+    always @(posedge clk)
+        case({j,k})
+            2'b00 : Q <= Q;
+            2'b01 : Q <= 0;
+            2'b10 : Q <= 1;
+            2'b11 : Q <= ~Q;
+            default : Q <= Q;
+        endcase
+    
+endmodule
