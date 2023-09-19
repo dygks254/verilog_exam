626e7c273dda9e7e0978223d62cac702ab46af41
YohabPark <dyqkr@naver.com>
2023-09-20 02:15:56 +0900

    Add group 14 4

---
+++ hdlbits-exam/group-14/exam14-4/top_module.v
@@ -0,0 +1,36 @@
+module top_module(
+    input in,
+    input [1:0] state,
+    output [1:0] next_state,
+    output reg out); 
+
+    parameter A=0, B=1, C=2, D=3;
+    
+    always @(*)begin
+        case(state)
+            A : begin
+                next_state <= in ? B : A;
+                out <= 0;
+            end
+            B : begin
+                next_state <= in ? B : C;
+                out <= 0;
+            end
+            C : begin
+                next_state <= in ? D : A;
+                out <= 0;
+            end
+            D : begin
+                next_state <= in ? B : C; 
+                out <= 1;
+            end
+            default : begin 
+                next_state <= A;
+                out <= 0;
+            end
+        endcase
+    end
+    
+    
+
+endmodule
