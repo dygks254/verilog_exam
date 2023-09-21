1de5ec8d5d7871073372f83b90c650b1b3d56722
YohabPark <dyqkr@naver.com>
2023-09-22 02:23:36 +0900

    Add group 14 6 tmp

---
+++ hdlbits-exam/group-14/exam14-6/top_module.v
@@ -0,0 +1,26 @@
+module top_module(
+    input clk,
+    input in,
+    input areset,
+    output out); //
+
+    parameter A=0, B=1, C=2, D=3;
+
+    wire [3:0] next_state;
+    reg  [3:0] state;
+    
+    assign next_state[A] = ~in & ( state[0] | state[2] );
+    assign next_state[B] =  in & ( |{state[3],state[1:0]} );
+    assign next_state[C] = ~in & ( state[1] | state[3] );
+    assign next_state[D] =  in & state[2];
+    
+    always @(posedge clk, negedge areset)begin
+        if(!areset)
+            state <= 'h0;
+        else
+            state <= next_state;
+    end
+    
+    assign out = state[D] == 'b1 ? 'b1 : 'b0;
+
+endmodule
