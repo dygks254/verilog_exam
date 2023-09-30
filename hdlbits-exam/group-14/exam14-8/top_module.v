d4a91459da21210a98bc1c4eaed52af8c4772449
YohabPark <dyqkr@naver.com>
2023-09-30 23:33:33 +0900

    Add group 14 8

--- hdlbits-exam/group-14/exam14-8/top_module.v
+++ hdlbits-exam/group-14/exam14-8/top_module.v
@@ -1,34 +1,45 @@
 module top_module (
-    input clk,
-    input reset,
-    input [3:1] s,
-    output fr3,
-    output fr2,
-    output fr1,
-    output dfr
-); 
+	input clk,
+	input reset,
+	input [3:1] s,
+	output reg fr3,
+	output reg fr2,
+	output reg fr1,
+	output reg dfr
+);
 
-    reg [2:0] all_buff;
-    assign {fr3, fr2, fr1} = all_buff;
-    
-    always @(posedge clk)begin
-        if(reset)
-            all_buff <= 0;
-        else
-            case(s)
-                3'b111 : all_buff <= 3'b000;
-                3'b011 : all_buff <= 3'b001;
-                3'b001 : all_buff <= 3'b011;
-                3'b000 : all_buff <= 3'b111;
-                default : all_buff <= 3'b000; 
-            endcase
-    end
-    
-    reg [3:1] buff_s;
-    always @(posedge clk)begin
-       buff_s <= all_buff; 
-    end
-    
-    assign dfr = all_buff >= buff_s ? 1'b1 : 1'b0;
-    
+	parameter A2=0, B1=1, B2=2, C1=3, C2=4, D1=5;
+	reg [2:0] state, next;
+
+	always @(posedge clk) begin
+		if (reset) state <= A2;
+		else state <= next;
+	end
+
+
+	always@(*) begin
+		case (state)
+			A2: next = s[1] ? B1 : A2;
+			B1: next = s[2] ? C1 : (s[1] ? B1 : A2);
+			B2: next = s[2] ? C1 : (s[1] ? B2 : A2);
+			C1: next = s[3] ? D1 : (s[2] ? C1 : B2);
+			C2: next = s[3] ? D1 : (s[2] ? C2 : B2);
+			D1: next = s[3] ? D1 : C2;
+			default: next = 3'bxxx;
+		endcase
+	end
+	
+	
+	always@(*) begin
+		case (state)
+			A2: {fr3, fr2, fr1, dfr} = 4'b1111;
+			B1: {fr3, fr2, fr1, dfr} = 4'b0110;
+			B2: {fr3, fr2, fr1, dfr} = 4'b0111;
+			C1: {fr3, fr2, fr1, dfr} = 4'b0010;
+			C2: {fr3, fr2, fr1, dfr} = 4'b0011;
+			D1: {fr3, fr2, fr1, dfr} = 4'b0000;
+			default: {fr3, fr2, fr1, dfr} = 4'bxxxx;
+		endcase
+	end
+	
 endmodule
