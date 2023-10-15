f0a506c5f5c94c655661ef92c8548d6ed074b4e8
dygks254 <dyqkr@naver.com>
2023-10-16 02:51:05 +0900

    Add group 14 17

--- hdlbits-exam/group-14/exam14-17/top_module.v
+++ hdlbits-exam/group-14/exam14-17/top_module.v
@@ -1,44 +1,52 @@
 module top_module(
     input clk,
     input in,
-    input reset,    // Synchronous reset
-    output reg [7:0] out_byte,
+    input reset,
+    output [7:0] out_byte,
     output done
-); //
+); 
+    reg [3:0] num8;
+    parameter RUNNING=0, DONE=1, IDLE=3, ERROR=4;
+    reg [2:0] state, next_state;
+    reg [7:0] data;
 
-    parameter ST0=1, ST1=2, ST2=4, ST3=8, ST4=16, ST5=32, ST6=64, ST7=128, ST8=256, ST9=512, ST10=1024;
-    reg [10:0] state, next_state;
-    reg [7:0] buff_out_byte;
-
-    always @(*)begin
-        case(state)
-            ST0 :   if(reset)   next_state <= ST0;
-                    else        next_state <= ST1;
-            ST1 :   next_state <= ST2;
-            ST2 :   next_state <= ST3;
-            ST3 :   next_state <= ST4;
-            ST4 :   next_state <= ST5;
-            ST5 :   next_state <= ST6;
-            ST6 :   next_state <= ST7;
-            ST7 :   next_state <= ST8;
-            ST8 :   next_state <= ST9;
-            ST9 :   next_state <= ST10;
-            ST10:   next_state <= ST1;
-            default : next_state <= ST0;
+    always @(*) begin
+        case (state)
+            IDLE: next_state <= in ? IDLE : RUNNING;
+            RUNNING: begin
+                if((num8==8)&in) begin
+                    next_state <= DONE;
+                end else if((num8==8)&(~in)) begin
+                    next_state <= ERROR;
+                end else begin
+                    next_state <= RUNNING;
+                    data[num8] <= in;
+                end
+            end
+            DONE: begin
+                next_state <= in ? IDLE : RUNNING;
+                out_byte <= data;
+            end
+            ERROR: next_state <= in ? IDLE : ERROR;
         endcase
     end
 
-    always @(posedge clk)
-        if(reset)
-            state <= ST0;
-        else
+    always @(posedge clk) begin
+        if(reset) begin
+            state <= IDLE;
+            num8 <= 0;
+        end else begin
+            if((state == RUNNING)&&(num8!=8)) begin
+                num8 <= num8 + 1;
+            end else if(state == ERROR) begin
+                num8 <= 0;
+            end else if(state == DONE) begin
+                num8 <= 0;
+            end
             state <= next_state;
-
-    always @(posedge clk)begin
-        buff_out_byte <= {buff_out_byte[6:0], in};
-        out_byte <= buff_out_byte;
+        end
     end
+    assign done = (state == DONE);
 
-    assign done = (state == ST10);
-
+    
 endmodule
